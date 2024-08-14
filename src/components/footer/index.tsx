327af58955d6fbc40725cb95a8cb126c960ba82a
Felipe Sachetto <felipesachetto2004@gmail.com>
2024-08-14 10:02:58 -0300

    finalizou NavBar e Footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,10 +4,10 @@ import { Facebook, Instagram, Linkedin } from "lucide-react"
 
 export default function Footer(){
     return(
-        <div className="flex flex-wrap md:flex-col mx-auto bg-[#911F27] text-center w-full mt-24 py-8 min-h[250px] ">
+        <div className="flex flex-wrap mx-auto bg-[#911F27] text-center w-full mt-24 py-8 min-h[250px] items-start justify-between">
             <div className="px-8 flex flex-col text-[#F2EBD5] items-start">
                 <p className="text-2xl underline">Sobre nós</p>
-                <div className="flex flex-col items-start">
+                <div className="flex flex-col items-start ">
                     <Link href="/" target="_blank" > Visão </Link>
                     <Link href="/" target="_blank"> Missão </Link>   
                     <Link href="/" target="_blank"> Valores </Link> 
@@ -22,8 +22,8 @@ export default function Footer(){
                     className="max-w-[14rem] lg:max-w-sm opacity-80 hover:opacity-100 transition-all"
                 />
             </div>
-            <div className="flex flex-col items-end px-8">
-                <p className="text-[#F2EBD5] text-2xl underline">Redes Sociais</p>
+            <div className="flex flex-col px-8 justify-start">
+                <p className="text-[#F2EBD5] text-2xl underline flex items-end">Redes Sociais</p>
                 <div className="flex flex-wrap gap-4">
                     <Link href="/"><Instagram className="text-[#F2EBD5]"/></Link>
                     <Link href='/'><Linkedin className="text-[#F2EBD5]"/></Link>
