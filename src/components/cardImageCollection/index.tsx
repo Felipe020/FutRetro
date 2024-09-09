359a769f21a861cdfa283e2f0b5e1886227dccda
Felipe Sachetto <felipesachetto2004@gmail.com>
2024-09-09 17:36:00 -0300

    fixing more errors

--- src/components/cardImageCollection/index.tsx
+++ src/components/cardImageCollection/index.tsx
@@ -16,7 +16,7 @@ export default function CardImageCollection({
   return (
     <div className="flex flex-col ">
       <div className="flex w-full max-w-[346px] h-auto xl:max-w-[446px] xl:h-auto bg-[#911F27] rounded-t-3xl m-auto justify-center">
-        <Link href={`/product/${product.id}`}>
+        <Link href={`/product/${product?.id}`}>
           <Image
             src={imageSrc}
             alt={imageAlt}
@@ -27,8 +27,8 @@ export default function CardImageCollection({
             height={450}
           />
           <div className="flex flex-col bg-[#F2EBD5] rounded-b-3xl justify-center items-center p-2 w-[346px] xl:w-[446px] m-auto">
-            <p className="text-3xl text-[#911F27] font-bold">{product.title}</p>
-            <p className="text-3xl text-[#911F27] font-bold">{product.price}</p>
+            <p className="text-3xl text-[#911F27] font-bold">{product?.title}</p>
+            <p className="text-3xl text-[#911F27] font-bold">{product?.price}</p>
           </div>
         </Link>
       </div>
